1ee0c504ff0377a9da243ffcab9f2ac082168b9c
aligniqsystem <aligniqsystem@gmail.com>
2026-04-01 20:22:06 -0400

    Create data.js
    add data file for tracking system results

---
+++ data.js
@@ -0,0 +1,22 @@
+export const data = {
+  record: "0-0",
+  winRate: "0%",
+  roi: "0%",
+
+  todaysPlays: [
+    {
+      matchup: "Lakers vs Suns",
+      result: null
+    }
+  ],
+
+  results: [
+    {
+      date: "April 1",
+      games: [
+        { matchup: "Lakers vs Suns", result: "win" },
+        { matchup: "Celtics vs Heat", result: "loss" }
+      ]
+    }
+  ]
+};
